3a46f7f6128cdc2f93dee6181c47e12b9ec7b716
NamgyungKim <namgyung.kim@gmail.com>
2021-10-11 12:13:54 +0900

    Lv1: 가운데 글자 가져오기

---
+++ Lv.1/12903.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  let answer = ''
+  if (s.split('').length % 2 == 1) {
+    answer = s.split('')[Math.floor(s.split('').length / 2)]
+  } else {
+    answer = s.split('')[s.split('').length / 2 - 1]
+    answer += s.split('')[s.split('').length / 2]
+  }
+  return answer
+}
+
+// substr을 사용했으면 더 줄어들었을텐데...;;
