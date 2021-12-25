189bcb5ee52bdc554b195cbd95c6e1f24bff11a0
NamgyungKim <namgyung.kim@gmail.com>
2021-12-26 02:13:29 +0900

    12.25

--- Lv.1/12906.js
+++ Lv.1/12906.js
@@ -1,14 +1,8 @@
 // 같은 숫자는 싫어
+
 function solution(arr){
-    var answer = [];
-    for(let i = 0; i<arr.length; i++){
-        // answer배열의 맨 마지막 값과 arr[i]값이 같은가?
-        // 같지않으면 arr[i] push
-        if(answer[answer.length-1] !== arr[i]){
-            answer.push(arr[i])
-        }
-    }
-    return answer;
+    // 바로 이전의 배열과 비교 후 다르면 return
+    return arr.filter((num, index) => arr[index-1] !== num)
 }
 
-solution([1,1,3,3,0,1,1])
+console.log(solution([1,1,3,3,0,1,1]))
--- Lv.1/12910.js
+++ Lv.1/12910.js
@@ -1,20 +1,11 @@
 // 나누어 떨어지는 숫자 배열
 
-
-// arr의 각 숫자를 divisor로 나누어 떨어지는 값을 오름차순으로 정렬
-
 function solution(arr, divisor) {
-    var answer = [];
-		// 나누어서 떨어지면 answer에 push
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i]%divisor == 0){answer.push(arr[i])}
-    }
-		// 만약 나누어떨어지는 숫자가 없으면 answer=[-1] 을 줌
-    if(answer == ''){answer=[-1]}
-		// answer를 오름차순으로 정렬
-    answer.sort(function(a,b){return a - b})
-    console.log(answer)
-    return answer;
+  // 각배열의 값 % divisor == 0 인것들만 answer 배열로 반환
+  let answer = arr.filter((num)=> num%divisor === 0)
+  return answer == '' 
+  // 빈배열이면 [-1] 반환
+  ? [-1] 
+  // 빈배열이 아니면 answer을 오름차순으로 반환
+  : answer.sort(function(a,b){return a - b})
 }
-
-solution([5, 9, 7, 10],19)
